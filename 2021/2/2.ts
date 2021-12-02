373f48f3dd6eae9c2b78be9cc1b9a459f89be64c
Nick Potter <nick.potter@receipt-bank.com>
2021-12-02 08:36:26 +0000

    Day 2 solution

---
+++ 2021/2/2.ts
@@ -0,0 +1,50 @@
+import * as fs from 'fs';
+
+function getPosition(positions: string[]) : number {
+    let pos = [0, 0, 0];
+
+    for (let i = 0; i < positions.length; i++) {
+        let directions = positions[i].split(' ');
+
+        switch (directions[0]) {
+            case 'forward':
+                pos[0] = pos[0] + Number(directions[1]);
+                break;
+            case 'down':
+                pos[1] = pos[1] + Number(directions[1]);
+                break;
+            case 'up':
+                pos[1] = pos[1] - Number(directions[1]);
+                break;
+        }
+    } 
+
+    return pos[0] * pos[1];
+}
+
+function getPosition2(positions: string[]) : number {
+    let pos = [0, 0, 0];
+
+    for (let i = 0; i < positions.length; i++) {
+        let directions = positions[i].split(' ');
+
+        switch (directions[0]) {
+            case 'forward':
+                pos[0] = pos[0] + Number(directions[1]);
+                pos[1] = pos[1] + pos[2] * Number(directions[1]);
+                break;
+            case 'down':
+                pos[2] += Number(directions[1]);
+                break;
+            case 'up':
+                pos[2] -= Number(directions[1]);
+                break;
+        }
+    } 
+
+    return pos[0] * pos[1];
+}
+
+const file = fs.readFileSync('./2021/2/input.txt','utf8').toString();
+const nums = file.split("\n");
+console.log(getPosition2(nums));
