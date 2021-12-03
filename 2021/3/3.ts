28862391d3af890602156eeb8c242ced1077f9a6
Nick Potter <nick.potter@receipt-bank.com>
2021-12-03 08:43:26 +0000

    Day 3 part 2 solution

--- 2021/3/3.ts
+++ 2021/3/3.ts
@@ -25,15 +25,46 @@ function getGammaAndEpsilon(nums: string[]) : number {
 }
 
 
-function e2(nums: string[]) {
-    // examine first bit
-    // keep numbers selected by bit criteria
-    // when one number left, this is the rating value
-    // move to the next bit on the right
+function getLifeSupport(nums: string[]) : number {
+    let mostCommon: number;
+    let leastCommon: number;
+    let oxygenRating: string[] = nums;
+    let co2Rating: string[] = nums;
 
-    // oxygen generator: bit == most common value in bit pos or 1 if equal
-    // co2 scrubber: bit == least common value in bit pos or 0 if equal
+    for (let col = 0; col < nums[0].length; col++) {
+        let zeroCount = [0, 0];
+        let oneCount = [0, 0];
+
+        for (let row of oxygenRating) {
+            if (Number(row[col]) == 0) {
+                zeroCount[0]++;
+            }
+            else {
+                oneCount[0]++;
+            }
+        }
+
+        for (let row of co2Rating) {
+            if (Number(row[col]) == 0) {
+                zeroCount[1]++;
+            }
+            else {
+                oneCount[1]++;
+            }
+        }
+
+        mostCommon = (zeroCount[0] <= oneCount[0]) ? 1 : 0;
+        leastCommon = (zeroCount[1] <= oneCount[1]) ? 0 : 1;
+
+        oxygenRating = oxygenRating.length > 1 ? oxygenRating.filter(e => e.charAt(col) == mostCommon.toString()) : oxygenRating;
+        co2Rating = co2Rating.length > 1 ? co2Rating.filter(e => e.charAt(col) == leastCommon.toString()) : co2Rating;
+    }
+
+    var oxy = parseInt(oxygenRating[0], 2);
+    var co2 = parseInt(co2Rating[0], 2)
+    return oxy * co2;
 }
 
-const nums = fs.readFileSync('./2021/3/input_example.txt','utf8').toString().split("\n");
+const nums = fs.readFileSync('./2021/3/input.txt','utf8').toString().split("\n");
 console.log(getGammaAndEpsilon(nums));
+console.log(getLifeSupport(nums));
