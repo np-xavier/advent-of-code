03087e6c1ec23692a8f3881ab51f597d0b0404c1
Nick Potter <nick.potter@receipt-bank.com>
2021-12-04 19:08:49 +0000

    Terrible solution for day 4

--- 2021/4/4.ts
+++ 2021/4/4.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const nums = fs.readFileSync('./2021/4/input_example.txt','utf8').toString().split("\n\n");
+const nums = fs.readFileSync('./2021/4/input.txt','utf8').toString().split("\n\n");
 
 function getSum(board: string[][]) : number {
     let sum = 0;
@@ -37,6 +37,30 @@ function findWin(choices: string, boards: string[][][]) : any {
     }
 }
 
+function findWin2(choices: string, boards: string[][][]) : any {
+    for (let choice of choices.split(",")) {
+        for (let i = 0; i < boards.length; i++) {
+            for (let j = 0; j < boards[i].length; j++) { 
+                for (let k = 0; k < boards[i][j].length; k++) {
+                    boards[i][j][k] = (boards[i][j][k] == choice) ? "-1" : boards[i][j][k];
+                    if (boards[i][j].every(e => e == '-1')) {
+                        let a = { board: boards[i], choice: choice };
+                        boards.splice(i, 1);
+                        return a;
+                    }
+                    for (let x = 0; x < 5; x++) {
+                        if (boards[i].every(e => e[x] == '-1')) {
+                            let a = { board: boards[i], choice: choice };
+                            boards.splice(i, 1);
+                            return a;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 let choices = nums[0];
 let bingo: string[][] = [];
 let rows: string[][] = [];
@@ -61,6 +85,11 @@ for (let i = 0; i < rows.length; i += window) {
 
 groupedBingo.pop();
 
-let result = findWin(choices, groupedBingo);
+let result = findWin2(choices, groupedBingo);
+
+while (groupedBingo.length > 0) {
+    result = findWin2(choices, groupedBingo);
+}
+
 console.log(getSum(result.board), result.choice);
 console.log(getSum(result.board) * result.choice);
