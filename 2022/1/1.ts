92f8fa4896d2b9e08a21282edd143e7f66def550
Nick Potter <nick.potter@receipt-bank.com>
2022-12-01 14:04:29 +0000

    Refine day 1 even futher

--- 2022/1/1.ts
+++ 2022/1/1.ts
@@ -1,18 +1,15 @@
 import * as fs from 'fs';
 
-const calorieTotalStrings = fs.readFileSync('./2022/1/input.txt', 'utf8').split('\n\n').map(e => e.split('\n')); // Break down into multiarray
-const calorieTotals = calorieTotalStrings.map(e => e.map(s => parseInt(s))); // Convert entries to numbers
-
 function getBestTotals(calorieTotals: Array<Array<number>>, topCount: number) {
-    let bestTotals: Array<number> = [];
-
-    for (let item of calorieTotals) {
-        bestTotals.push(item.reduce((p, c) => p + c));
-        bestTotals = bestTotals.sort((a, b) => a - b).slice(-topCount);    
-    }
-
-    return bestTotals;
+    return calorieTotals.map(e => e.reduce((p, c) => p + c))
+                        .sort((a, b) => a - b)             
+                        .slice(-topCount);
 }
+
+const calorieTotals = fs.readFileSync('./2022/1/input.txt', 'utf8')
+                        .split('\n\n')
+                        .map(e => e.split('\n')
+                        .map(s => parseInt(s)));
 
 console.log(getBestTotals(calorieTotals, 1));
 console.log(getBestTotals(calorieTotals, 3));
--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Run `npm install`
 
 Run `npx tsc` from the root folder to compile your `.ts` files to `.js` (see `tsconfig.json` for the watched folders)
 
+Or `npx tsc -w` to watch
+
 # Run the script
 
 Run `node ./20XX/<number>/<number>.js` from the root folder to execute a particular script
