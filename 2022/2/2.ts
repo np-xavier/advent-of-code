2abfffe48c0b31a996f0f5448f5440fb8b8543d7
Nick Potter <nick.potter@receipt-bank.com>
2022-12-02 09:08:45 +0000

    Further refinement of day 2

--- 2022/2/2.ts
+++ 2022/2/2.ts
@@ -1,64 +1,47 @@
 import * as fs from 'fs';
 
-function calculateInitialStratScore(roundStrats: Array<Array<string>>) {
+function calculateStratScore(roundStrats: Array<Array<string>>, first = true) {
     let totalScore = 0;
+    let data: any = {
+        'A': { loses: 'Y', draws: 'X', wins: 'Z' },
+        'B': { loses: 'Z', draws: 'Y', wins: 'X' },
+        'C': { loses: 'X', draws: 'Z', wins: 'Y' },
+        'X': 1,
+        'Y': 2,
+        'Z': 3
+    };
 
-    for (let entry of roundStrats) {
-        if (entry[1] === oppData[entry[0]].loses) {
-            totalScore += pointData[entry[1]] + 6;
-            console.log('win', totalScore);
-        }
-        else if (entry[1] === oppData[entry[0]].draws) {
-            totalScore += pointData[entry[1]] + 3;
-            console.log('draw', totalScore);
-        }
-        else {
-            totalScore += pointData[entry[1]];
-            console.log('lose', totalScore);
+    if (first) {
+        for (let entry of roundStrats) {
+            if (entry[1] === data[entry[0]].loses) {
+                totalScore += data[entry[1]] + 6;
+            }
+            else if (entry[1] === data[entry[0]].draws) {
+                totalScore += data[entry[1]] + 3;
+            }
+            else {
+                totalScore += data[entry[1]];
+            }
         }
     }
-
-    return totalScore;
-}
-
-function calculateFinalStratScore(roundStrats: Array<Array<string>>) {
-    let totalScore = 0;
-
-    for (let entry of roundStrats) {
-        if (entry[1] === 'Z') {
-            totalScore += pointData[oppData[entry[0]].loses] + 6;
-            console.log('win', totalScore);
-        }
-        else if (entry[1] === 'Y') {
-            totalScore += pointData[oppData[entry[0]].draws] + 3;
-            console.log('draw', totalScore);
-        }
-        else {
-            totalScore += pointData[oppData[entry[0]].wins];
-            console.log('lose', totalScore);
+    else {
+        for (let entry of roundStrats) {
+            if (entry[1] === 'Z') {
+                totalScore += data[data[entry[0]].loses] + 6;
+            }
+            else if (entry[1] === 'Y') {
+                totalScore += data[data[entry[0]].draws] + 3;
+            }
+            else {
+                totalScore += data[data[entry[0]].wins];
+            }
         }
     }
-
+    
     return totalScore;
 }
-
-// Rock: A, Paper: B, Scissors: C
-// Rock: X, Paper: Y, Scissors: Z 
-// X: Lose, Y: Draw, Z: Win
 
 const roundStrats = fs.readFileSync('./2022/2/input.txt', 'utf8').split('\n').map(e => e.split(' '));
 
-let oppData: any = {
-    'A': { loses: 'Y', draws: 'X', wins: 'Z' },
-    'B': { loses: 'Z', draws: 'Y', wins: 'X' },
-    'C': { loses: 'X', draws: 'Z', wins: 'Y' }
-};
-
-let pointData: any = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3
-};
-
-console.log(calculateInitialStratScore(roundStrats));
-console.log(calculateFinalStratScore(roundStrats));
+console.log(calculateStratScore(roundStrats));
+console.log(calculateStratScore(roundStrats, false));
