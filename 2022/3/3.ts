2f6766793eb52e358e79393563216ac032d2434e
Nick Potter <nick.potter@receipt-bank.com>
2022-12-05 18:17:55 +0000

    Refined solution for day 3

--- 2022/3/3.ts
+++ 2022/3/3.ts
@@ -3,34 +3,26 @@ import * as fs from 'fs';
 const items = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 function getPriorities(input: Array<string>) {
-    let total = 0;
-
-    for (let row of input) {
-        let S1 = new Set(row.slice(0, row.length / 2));
-        let S2 = new Set(row.slice(row.length / 2));
+    return input.map(x => {
+        let S1 = new Set(x.slice(0, x.length / 2));
+        let S2 = new Set(x.slice(x.length / 2));
         let intersect = new Set([...S1].filter(s => S2.has(s)));
-        total += (items.indexOf(intersect.values().next().value) + 1);
-    }
-
-    return total;
+        return items.indexOf(intersect.values().next().value) + 1;
+    }).reduce((a, c) => a + c);
 }
 
 function getBadgePriorities(input: Array<string>) {
-    let total = 0;
+    let groups = input.map((e, i, a) => i % 3 == 0 ? a.slice(i, i+3) : '').filter(e => e !== '');
 
-    console.log(input);
-    for (let i = 0; i < input.length; i += 3) {
-        let S1 = new Set(input[i]);
-        let S2 = new Set(input[i+1]);
-        let S3 = new Set(input[i+2]);
+    return groups.map((x) => {
+        let S1 = new Set(x[0]);
+        let S2 = new Set(x[1]);
+        let S3 = new Set(x[2]);
         let intersect = [S1, S2, S3].reduce((a, b) => new Set([...a].filter(x => b.has(x))));
-        total += (items.indexOf(intersect.values().next().value) + 1);
-        console.log(intersect);
-    }
-
-    return total;
+        return items.indexOf(intersect.values().next().value) + 1;
+    }).reduce((a, c) => a + c);
 }
 
-const input = fs.readFileSync('./2022/3/input.txt', 'utf8').split('\n');
+const input = fs.readFileSync('./2022/3/input_example.txt', 'utf8').split('\n');
 console.log(getPriorities(input));
 console.log(getBadgePriorities(input));
